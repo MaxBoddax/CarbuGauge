550e152419b38f35fca3d29bbc79ec1a8c13d191
MaxBoddax <134828481+MaxBoddax@users.noreply.github.com>
2023-05-31 05:29:57 +0200

    Add files via upload

---
+++ VHDL/led_bars.vhd
@@ -0,0 +1,78 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity led_bars is
+    port (
+        clk : in std_logic;
+        reset_n : in std_logic;
+        adc_data_ch0 : in std_logic_vector(15 downto 0);
+        adc_data_ch1 : in std_logic_vector(15 downto 0);
+        adc_data_ch2 : in std_logic_vector(15 downto 0);
+        adc_data_ch3 : in std_logic_vector(15 downto 0);
+        led_bar_out : out std_logic_vector(29 downto 0)
+    );
+end entity led_bars;
+
+architecture behavioral of led_bars is
+    signal peak_hold_ch0 : unsigned(15 downto 0) := (others => '0');
+    signal peak_hold_ch1 : unsigned(15 downto 0) := (others => '0');
+    signal peak_hold_ch2 : unsigned(15 downto 0) := (others => '0');
+    signal peak_hold_ch3 : unsigned(15 downto 0) := (others => '0');
+    signal peak_counter : integer := 0;
+begin
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if reset_n = '0' then
+                -- Reset all values
+                peak_hold_ch0 <= (others => '0');
+                peak_hold_ch1 <= (others => '0');
+                peak_hold_ch2 <= (others => '0');
+                peak_hold_ch3 <= (others => '0');
+                peak_counter <= 0;
+                led_bar_out <= (others => '0');
+            else
+                -- Update peak hold values
+                if unsigned(adc_data_ch0) >= peak_hold_ch0 then
+                    peak_hold_ch0 <= unsigned(adc_data_ch0);
+                    peak_counter <= 250;
+                elsif peak_counter = 0 then
+                    peak_hold_ch0 <= (others => '0');
+                else
+                    peak_counter <= peak_counter - 1;
+                end if;
+                
+                if unsigned(adc_data_ch1) >= peak_hold_ch1 then
+                    peak_hold_ch1 <= unsigned(adc_data_ch1);
+                    peak_counter <= 250;
+                elsif peak_counter = 0 then
+                    peak_hold_ch1 <= (others => '0');
+                else
+                    peak_counter <= peak_counter - 1;
+                end if;
+                
+                if unsigned(adc_data_ch2) >= peak_hold_ch2 then
+                    peak_hold_ch2 <= unsigned(adc_data_ch2);
+                    peak_counter <= 250;
+                elsif peak_counter = 0 then
+                    peak_hold_ch2 <= (others => '0');
+                else
+                    peak_counter <= peak_counter - 1;
+                end if;
+                
+                if unsigned(adc_data_ch3) >= peak_hold_ch3 then
+                    peak_hold_ch3 <= unsigned(adc_data_ch3);
+                    peak_counter <= 250;
+                elsif peak_counter = 0 then
+                    peak_hold_ch3 <= (others => '0');
+                else
+                    peak_counter <= peak_counter - 1;
+                end if;
+                
+                -- Update LED bar output
+                led_bar_out <= std_logic_vector(resize(peak_hold_ch3, 10) & resize(peak_hold_ch2, 10) & resize(peak_hold_ch1, 10) & resize(peak_hold_ch0, 10));
+            end if;
+        end if;
+    end process;
+end architecture behavioral;
---
+++ VHDL/vumeter.vhd
@@ -0,0 +1,163 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY vumeter IS
+	PORT (
+		clk : IN STD_LOGIC; --system clock
+		reset_n : IN STD_LOGIC; -- reset addr_asm and address gen
+		start : IN STD_LOGIC;
+		-- write data to ADC pin
+		data_rd : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); -- Digital data from ADC channel
+		adc_data_ready : OUT STD_LOGIC; -- Signal indicating that ADC data is ready
+		pga : IN std_logic_vector(2 DOWNTO 0);
+		dr : IN std_logic_vector(2 DOWNTO 0);
+		--i2c_clk : out std_logic;
+		sda : INOUT STD_LOGIC; --serial data output of I2C bus
+		scl : INOUT STD_LOGIC; --serial clock output of I2C bus
+		ch_sel : in  STD_LOGIC
+	);
+END vumeter;
+
+ARCHITECTURE Behavioral OF vumeter IS
+
+	COMPONENT i2c_master IS
+		GENERIC (
+			input_clk : INTEGER :=  25000000; --input clock speed from user logic in Hz
+			bus_clk : INTEGER := 400000
+		);
+		PORT (
+			clk : IN STD_LOGIC; --system clock
+			reset_n : IN STD_LOGIC; --active low reset
+			ena : IN STD_LOGIC; --latch in command
+			addr : IN STD_LOGIC_VECTOR(6 DOWNTO 0); --address of target slave
+			rw : IN STD_LOGIC; --'0' is write, '1' is read
+			data_wr : IN STD_LOGIC_VECTOR(7 DOWNTO 0); --data to write to slave
+			busy : OUT STD_LOGIC; --indicates transaction in progress
+			finish : OUT std_logic;
+			data_rd : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); --data read from slave
+			ack_error : BUFFER STD_LOGIC; --flag if improper acknowledge from slave
+			sda : INOUT STD_LOGIC; --serial data output of I2C bus
+			scl : INOUT STD_LOGIC --serial clock output of I2C bus
+		);
+	END COMPONENT;
+
+	COMPONENT addr_asm IS
+		PORT (
+			clk, reset_n : IN STD_LOGIC;
+			start, cmd_done : IN STD_LOGIC;
+			addr_out : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+			data_wr : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			ena, rw, send : OUT std_logic;
+			pga : IN std_logic_vector(2 DOWNTO 0);
+			dr : IN std_logic_vector(2 DOWNTO 0);
+			data_raw_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			data_rd : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+		);
+	END COMPONENT;
+
+	SIGNAL clk_50 : STD_LOGIC;
+	SIGNAL ena : STD_LOGIC;
+	SIGNAL addr : STD_LOGIC_VECTOR(6 DOWNTO 0);
+	SIGNAL rw : STD_LOGIC;
+	SIGNAL data_wr : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL busy, send : STD_LOGIC;
+	SIGNAL data_rd_signal : STD_LOGIC_VECTOR(15 DOWNTO 0);
+	SIGNAL adc_data : STD_LOGIC_VECTOR(15 DOWNTO 0); -- Variable to store ADC data
+	SIGNAL ack_error : STD_LOGIC;
+	SIGNAL count : INTEGER RANGE 0 TO 8191 := 0;
+	SIGNAL finish : std_logic;
+
+	-- Signal frame_complete : std_logic; -- tell that all the design data is going to BRAM completely
+	TYPE rd_state IS (idle, send_addr);
+	SIGNAL state_rd : rd_state;
+
+	SIGNAL d_ready_sig, frame_complete_delay, start_one_clk, change_state_d : std_logic;
+	SIGNAL globalCounter : std_logic_vector(12 - 1 DOWNTO 0) := (OTHERS => '0');
+	signal data_rd_intermediate : std_logic_vector(15 downto 0); -- Sostituisci N con la larghezza del segnale data_rd
+BEGIN
+	-- data_rd <= x"0000" & data_rd_signal; -- Commented out since not using BRAM
+
+	--d_ready <= d_ready_sig;
+	i2c : i2c_master
+		GENERIC MAP (
+			input_clk =>  25000000, --input clock speed from user logic in Hz
+			bus_clk => 400000
+		)
+		PORT MAP (
+			clk => clk, 
+			reset_n => '1', 
+			ena => ena, 
+			addr => addr, 
+			rw => rw, 
+			data_wr => data_wr, 
+			busy => busy, 
+			data_rd => data_rd_signal, 
+			ack_error => ack_error, 
+			sda => sda, 
+			scl => scl, 
+		--	ch_sel => ch_sel,
+			finish => finish
+		);
+
+	address_gen : addr_asm
+		PORT MAP (
+		clk => clk, 
+		reset_n => '1', 
+		start => start, 
+		cmd_done => finish, 
+		addr_out => addr, 
+		data_wr => data_wr, 
+		ena => ena, 
+		rw => rw, 
+		send => send, 
+		pga => pga, 
+		dr => dr, 
+		data_raw_in => data_rd_intermediate, -- Utilizzo del segnale intermedio
+		data_rd => data_rd_signal
+	);
+
+-- Assegnazione del segnale intermedio
+data_rd_signal <= data_rd_intermediate;
+	start_one_clk <= start AND (NOT change_state_d);
+
+	PROCESS (clk)
+	BEGIN
+		IF (clk'EVENT AND clk = '1') THEN
+			change_state_d <= start;
+		END IF;
+	END PROCESS;
+
+	PROCESS (clk)
+	BEGIN
+		IF rising_edge(clk) THEN
+			CASE state_rd IS
+				WHEN idle => 
+					-- wait for start
+					count <= 0;
+					adc_data_ready <= '0'; -- Reset the data ready signal
+					IF start_one_clk = '1' THEN
+						state_rd <= send_addr;
+					ELSE
+						state_rd <= idle;
+					END IF;
+				
+				WHEN send_addr => 
+					IF count < 2048 THEN
+						IF send = '1' THEN
+							count <= count + 1;
+							state_rd <= send_addr;
+						ELSE
+							state_rd <= send_addr;
+						END IF;
+					ELSE
+						count <= 0;
+						state_rd <= idle;
+					END IF;
+			END CASE;
+		END IF;
+	END PROCESS;
+
+	data_rd <= adc_data; -- Assign the ADC data to the output signal
+
+END Behavioral;
